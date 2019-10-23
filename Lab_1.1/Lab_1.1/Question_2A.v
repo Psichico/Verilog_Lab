060b03dae1145de3cc45446e074d68677836046d
Psichico <jaimilpatel30@gmail.com>
2019-10-23 16:28:26 -0700

    Question 2A verified

--- Lab_1.1/Lab_1.1/Question_2A.v
+++ Lab_1.1/Lab_1.1/Question_2A.v
@@ -7,10 +7,10 @@ module Question_2A(x1, x2, x3, x4, f, a, b, c, d, e);
 	wire f,a,b,c,d,e;
 	
 	assign a = x1 & ~x3 ;
-	assign b = x2 | ~x3 ; 
-	assign c = ~x3 | ~x4 ;
-	assign d = x1 | x2;
-	assign e = x1 | ~x4;
+	assign b = x2 & ~x3 ; 
+	assign c = ~x3 & ~x4 ;
+	assign d = x1 & x2;
+	assign e = x1 & ~x4;
 	assign f = a | b | c | d | e;
 	
 
