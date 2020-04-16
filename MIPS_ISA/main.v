0c30d1a8535195fc03463e9582929390e44cd00b
Jaimil <jaimilpatel30@gmail.com>
2020-04-15 19:05:04 -0700

    before going to version 2

--- MIPS_ISA/main.v
+++ MIPS_ISA/main.v
@@ -45,6 +45,20 @@ reg [4:0] temp1, temp2, temp3;
 //reg [4:0] rd;
 //reg [10:0] shift_func; 
 
+//memory block. can be variable
+initial begin
+
+	memory[0] = 8'd0;
+	memory[1] = 8'd1;
+	memory[2] = 8'd2;
+	memory[3] = 8'd3;
+	memory[4] = 8'd4;
+	memory[5] = 8'd5;
+	memory[6] = 8'd6;
+	memory[7] = 8'd7;
+
+end
+
 always @ (posedge clock or posedge reset)
 begin
 	if(reset)
@@ -55,9 +69,11 @@ begin
 end
 
 wire [4:0] source1, source2;
+reg  [4:0] dest;
 
 assign source1 = memory[temp1] ;
 assign source2 = memory[temp2] ;
+//assign memory[temp3] = memory[temp3] ;
 
 ////combinational logic
 //always @ (*)
@@ -98,17 +114,73 @@ begin
 		s2:
 		begin
 			//execute
+			alu_out <= source1 + source2;
 			
 			
 			//Decode
 			op <= instruction_reg[31:26];
-			rs <= instruction_reg[25:21];
-			rt <= instruction_reg[20:16];
-			rd <= instruction_reg[15:11];
+			temp1 <= instruction_reg[25:21];
+			temp2 <= instruction_reg[20:16];
+			temp3 <= instruction_reg[15:11];
+			
+			nstate <= s3;
+		end
+		
+		s3:
+		begin
+			//memory state
+			dest <= alu_out;
+			
+			//execute
+			if(source1 == source2)
+			begin
+				//jump to done label
+			end
+			
+			else
+			begin
+				//fetch
+				instruction_reg <= instruction;
+				
+				nstate <= s4;
+			end
+		end
+		
+		s4:
+		begin
+			//writeback
+			memory[temp3] <= dest;
+			
+			//memory
+			
+			//decode
+			op <= instruction_reg[31:26];
+			temp1 <= instruction_reg[25:21];
+			temp2 <= instruction_reg[20:16];
+			temp3 <= instruction_reg[15:11];
+			
 			//fetch
 			instruction_reg <= instruction;
+				
+			nstate <= s5;
 			
-			nstate <= s2;
+		end
+		
+		s5:
+		begin
+			//writeback
+			
+			//execute of lw $r2 0($r3)
+			alu_out <= source1 + source2;
+			
+			//decode lw $r4 0($r5)
+			op <= instruction_reg[31:26];
+			temp1 <= instruction_reg[25:21];
+			temp2 <= instruction_reg[20:16];
+			temp3 <= instruction_reg[15:11];
+			
+			//
+			
 		end
 		
 		
